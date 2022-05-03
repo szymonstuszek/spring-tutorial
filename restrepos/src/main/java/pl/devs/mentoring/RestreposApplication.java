9f5f6398b0ca368e565fdd7ca057b0004d711207
szymon <szymonstuszek@gmail.com>
2022-05-03 09:44:30 +0200

    rest repositories

---
+++ restrepos/src/main/java/pl/devs/mentoring/RestreposApplication.java
@@ -0,0 +1,13 @@
+package pl.devs.mentoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestreposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestreposApplication.class, args);
+	}
+
+}
