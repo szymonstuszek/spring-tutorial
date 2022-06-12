9728ed62ac7a5de38bc8a18cd142d805ce342603
szymon <szymonstuszek@gmail.com>
2022-06-12 09:47:23 +0200

    querying db

--- jdbcservice/src/main/java/pl/devs/mentoring/JdbcServiceApplication.java
+++ jdbcservice/src/main/java/pl/devs/mentoring/JdbcServiceApplication.java
@@ -3,33 +3,34 @@ package pl.devs.mentoring;
 import java.sql.*;
 
 public class JdbcServiceApplication {
-
-	public static void main(String[] args) throws SQLException {
-				final String url = "jdbc:postgresql://localhost:5432/postgres";
-		final String user = "postgres";
-		final String password = "<your-pass>";
-		String sql = "SELECT * FROM \"Mentors\"";
+	private final static String URL = "jdbc:postgresql://localhost:5432/test-pgAdmin";
+	private final static String USER = "<username>";
+	private final static String PASSWORD = "<password>";
+	private final static String QUERY = "SELECT * FROM mentors";
 
 
-		Connection conn = null;
-		try {
-			conn = DriverManager.getConnection(url, user, password);
-			Statement statement = conn.createStatement();
+	public static void main(String[] args) {
+		try (Connection connection = DriverManager
+				.getConnection(URL, USER, PASSWORD)) {
 
+			Statement statement = connection.createStatement();
 
+			ResultSet resultSet = statement.executeQuery(QUERY);
 
-			ResultSet resultSet = statement.executeQuery(sql);
-			while (resultSet.next()) {
-				System.out.println(resultSet.getString("ID"));
-				System.out.println(resultSet.getString("name"));
-				System.out.println(resultSet.getString("surname"));
+			if (resultSet != null) {
+				int rowCount = 0;
+
+				while (resultSet.next()) {
+					System.out.println(resultSet.getString("ID"));
+					System.out.println(resultSet.getString("name"));
+					System.out.println(resultSet.getString("surname"));
+				}
+				System.out.println("Count of registered mentors: " + rowCount);
 			}
 
 
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
-		} finally {
-			conn.close();
 		}
 	}
 }
